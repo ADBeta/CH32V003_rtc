a8531b2369ff6cfc35aa6f1a82cbfa1088a15f8f
ADBeta <ashley.adbeta@gmail.com>
2024-10-05 20:05:26 +0100

    Interrupt testing and reset done

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 This demo code shows how to set the CH32V003 up to act as a Real Time Clock
 IC, via a 32.768KHz XTAL Oscillator (and later a regular xtal using the opamp)
+connected to `PD4`
+
+
+## Circuit
+`Circuit 1` uses a 32.768KHz XTAL Oscillator which provides an accurate square
+wave, directly to GPIO pin `PD4`.  
+:Circuit Image:
+
+`Circuit 2` TODO
 
 ----
 ADBeta (c) 2024
--- src/rtc.c
+++ src/rtc.c
@@ -4,18 +4,29 @@
 #include "ch32v003fun.h"
 #include <stdio.h>
 
-volatile uint16_t _rtc_ticks = 0x0000;
-volatile uint16_t _rtc_secs  = 0x0000;
+#define RTC_TICKS_PER_SEC 32768
+volatile uint32_t _rtc_ticks = 0x0000;
+volatile uint32_t _rtc_secs  = 0x0000;
 
 // External Interrupt IRQ Handler
 void EXTI7_0_IRQHandler(void) __attribute__((interrupt));
 void EXTI7_0_IRQHandler(void) 
 {
 	// If PD4 triggers, incriment the ticks counter.
-	// If this counter wraps to 0x0000, incriment seconds
+	// If this counter wraps, reset and incriment seconds
 	if(EXTI->INTFR & EXTI_Line4)
 	{
+		++_rtc_ticks;
 
+		// This just incriments seconds, which is not very useful.
+		// Replace with time managment function if needed. 
+		// NOTE: Each tick at 32.768KHz has 1464 cycles free
+		if(_rtc_ticks >= RTC_TICKS_PER_SEC)
+		{
+			_rtc_ticks = 0;
+			++_rtc_secs;
+		}
+		
 		// Acknowledge the interrupt
 		EXTI->INTFR = EXTI_Line4;
 	}
@@ -25,17 +36,11 @@ void EXTI7_0_IRQHandler(void)
 int main()
 {
 	SystemInit();
-
-	// NOTE: PD0 LED
 	
 	// Enable PORTD for the XTAL, and AFIO for Interrupts
 	RCC->APB2PCENR = RCC_APB2Periph_GPIOD | RCC_APB2Periph_AFIO;
 	// Set up PD4 as a floating input for the XTAL to pulse
-	//funPinMode(PD4, GPIO_CFGLR_IN_FLOAT);
-	funPinMode(PD4, GPIO_CFGLR_IN_PUPD);
-	funDigitalWrite(PD4, FUN_LOW);
-
-	funPinMode(PD0, GPIO_CFGLR_OUT_10Mhz_PP);
+	funPinMode(PD4, GPIO_CFGLR_IN_FLOAT);
 
 	// Configure PD4 as an interrupt
 	AFIO->EXTICR |= AFIO_EXTICR_EXTI4_PD;
@@ -49,6 +54,8 @@ int main()
 
 	while(1)
 	{
+		Delay_Ms(1000);
+		printf("%d\n", _rtc_ticks);
 	}
 
 }
